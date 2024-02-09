b114cd957b064eb7d8592e93baec3f22f0783bdd
Shrp <alex@sherpa.one>
2024-02-09 13:10:38 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import "./App.css";
 
 function App() {
+  const pizzas = [
+    {
+      name: "Margherita",
+      price: 7
+    },
+    {
+      name: "Quattro Stagioni",
+      price: 12
+    },
+    {
+      name: "Regina",
+      price: 9
+    },
+    {
+      name: "Capricciosa",
+      price: 11
+    }
+  ];
+
   return (
     <div className="App">
       <h1>Pizzas</h1>
+      <p>Le menu contient {pizzas.length} pizzas</p>
+
+      {pizzas.map((pizza, index) => (
+        <p onClick={() => console.log(pizza.price)} key={index}>
+          {pizza.name}
+        </p>
+      ))}
     </div>
   );
 }
